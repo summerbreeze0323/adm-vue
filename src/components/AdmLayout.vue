8657d9b01aaf3deaa958bc89b23a4782f89502e6
Julee Kim <summerbreeze0323@gmail.com>
2021-01-14 16:45:10 +0900

    - 공통 Header, nav 추가

--- src/components/AdmLayout.vue
+++ src/components/AdmLayout.vue
@@ -1,21 +1,83 @@
 <template>
-	<div class="adm_layout">
-		<Header></Header>
-		<div class="main">
+	<div class="adm_layout menu_expanded">
+		<Nav :show="isShowNav" @toggleNav="toggleNav"></Nav>
+		<div class="app_content">
+			<Header @toggleNav="toggleNav"></Header>
 			<router-view></router-view>
 		</div>
+		<div class="overlay" :class="{'show': isShowNav}" @click="toggleNav"></div>
 	</div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Nav from '@/components/Nav'
 
 export default {
 	name: 'AdmLayout',
-	components: { Header }
+	components: {
+		Header,
+		Nav
+	},
+	created() {
+		this.checkWindowWidth()
+
+		window.addEventListener('resize', () => {
+			this.checkWindowWidth()
+		});
+	},
+	data() {
+		return {
+			isShowNav: true
+		}
+	},
+	methods: {
+		toggleNav() {
+			this.isShowNav = !this.isShowNav
+		},
+		checkWindowWidth() {
+			if(window.innerWidth <= 991) {
+				this.isShowNav = false
+			}
+		}
+	}
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.adm_layout {
+	height: 100%;
 
+	&.menu_expanded {
+		.nav {
+			width: 260px;
+		}
+		.app_content {
+			margin-left: 260px;
+		}
+	}
+
+	.overlay {
+		display: none;
+		opacity: 0;
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 99;
+		width: 100%;
+		height: 100%;
+		background: rgba(34,41,47,.5);
+		transition: all .5s ease-in-out;
+	}
+
+	@media (max-width: 991px) {
+    &.menu_expanded .app_content {
+			margin-left: 0;
+		}
+		.overlay.show {
+			display: block;
+			opacity: 1;
+		}
+  }
+}
 </style>
