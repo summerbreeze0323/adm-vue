4d6b794fafefa8c82f7b79bbc13d8f40979f6580
Julee Kim <summerbreeze0323@gmail.com>
2021-01-21 16:08:29 +0900

    auth api 변경

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,21 +6,21 @@ import axios from 'axios'
 
 // 회원 가입
 function postRegister(data) {
-	return axios.post('/api/users/register', data)
+	return axios.post('/api/staffs/register', data)
 }
 
 // 로그인
 function postLogin(data) {
-  return axios.post('/api/users/login', data)
+  return axios.post('/api/staffs/login', data)
 }
 
 // 사용자 정보
 function getUserInfo() {
-  return axios.get('/api/users/auth')
+  return axios.get('/api/staffs/auth')
 }
 // 로그 아웃
 function getLogout() {
-  return axios.get('/api/users/logout')
+  return axios.get('/api/staffs/logout')
 }
 
 export default {
